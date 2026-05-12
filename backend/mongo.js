c116c1777546dcfa1eba5dddf68b5d20d65ee208
Chad Lambert <Chadchoo20@gmail.com>
2026-05-11 19:53:28 -0500

    day 3 middleware, order routes, mongoDb auto loging

--- backend/mongo.js
+++ backend/mongo.js
@@ -3,7 +3,8 @@ let db;
 async function connectMongo() {
     const client = new MongoClient(process.env.MONGO_URI);
     await client.connect();
-    db = client.db('MongoDB connected');
+     db = client.db('ecommerce_db');
+   console.log ('MongoDB connected');
 };
 function getMongo() {
     return db;
